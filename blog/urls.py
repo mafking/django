ab39fe020f5fbf6cbfc3bb6e284059d60b3fdb5a
mafking <mafsteve@gmail.com>
2022-02-12 13:12:28 +0100

    added more templates

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
        path('',views.post_list,name='post_list'),
        path('view/<int:pk>/', views.post_view, name='post_view'),
+       path('new/',views.new_post,name='new_post'),
+       path('view/<int:pk>/ediit', views.edit_post,name='edit_post'),
+       path('delete/<int:pk>',views.del_post,name='del_post')
 
 
     ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import Post
 from django.utils import timezone
+from .forms import Postform
 
 # Create your views here.
 def post_list(request):
@@ -10,3 +11,35 @@ def post_list(request):
 def post_view(request,pk):
 	post=get_object_or_404(Post,pk=pk)
 	return render(request,'blog/post_view.html',{'post':post})
+
+def new_post(request):
+	if request.method=="POST":
+		form=Postform(request.POST)
+		if form.is_valid():
+			post= form.save(commit=False)
+			post.author=request.user
+			post.published_date=timezone.now()
+			post.save()
+			return redirect('post_view',pk=post.pk)
+	else:form=Postform()
+	
+	return render(request,'blog/new_post.html',{'form':form})
+
+def edit_post(request,pk):
+	post=get_object_or_404(Post,pk=pk)
+	if request.method=='POST':
+		form = Postform(request.POST,instance=post)
+		if form.is_valid():
+			post=form.save(commit=False)
+			post.author=request.user
+			post.published_date=timezone.now()
+			post.save()
+			return redirect('post_view',pk=post.pk)
+	else:form=Postform(instance=post)
+	return render(request,'blog/new_post.html',{'form':form})
+
+
+def del_post(request,pk):
+	post=get_object_or_404(Post,pk=pk)
+	post.delete()
+	return redirect('post_list')
